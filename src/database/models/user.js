3c026457e86f76a83d168bdbf40f50126a937afd
Maykon Freitas <maykon17programmer@gmail.com>
2022-08-25 16:56:11 -0300

    feat: criando model User

---
+++ src/database/models/user.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password:DataTypes.STRING,
+    image:DataTypes.STRING
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId',
+      as: 'BlogPosts'
+    });
+  };
+
+  return User;
+};
