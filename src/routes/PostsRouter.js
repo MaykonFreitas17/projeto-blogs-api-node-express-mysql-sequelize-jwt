d6a626502fc8ab5531274198b016d556462d3aa8
Maykon Freitas <maykon17programmer@gmail.com>
2022-08-28 15:15:59 -0300

    feat: adiciona rota '/post' com method GET para listagem de blog posts

--- src/routes/PostsRouter.js
+++ src/routes/PostsRouter.js
@@ -6,6 +6,7 @@ const tokenValidate = require('../middlewares/tokenValidate');
 
 const PostController = require('../controllers/PostsController');
 
+router.get('/', tokenValidate, PostController.getAll);
 router.post('/', tokenValidate, PostController.create);
 
 module.exports = router;
